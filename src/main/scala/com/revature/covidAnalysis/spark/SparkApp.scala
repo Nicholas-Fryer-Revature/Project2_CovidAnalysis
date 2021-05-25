cec0808469ca44607ef2d0943308d77c7b870353
RubioRevatureProjects <ariel.rubioperez@revature.net>
2021-05-24 22:42:41 -0400

    created a directory package to hold the variable of our hdfs path(also added it to git ignore). Added to SparkApp SparkTest to read in a sample .csv file as a dataframe.

--- src/main/scala/com/revature/covidAnalysis/spark/SparkApp.scala
+++ src/main/scala/com/revature/covidAnalysis/spark/SparkApp.scala
@@ -1,12 +1,14 @@
 package com.revature.covidAnalysis.spark
 
+import directories.hdfsLocation
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{min, max, avg, expr, udf, count}
+import org.apache.spark.sql.functions.{avg, count, expr, max, min, udf}
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 import org.apache.spark.ml.feature.StringIndexer
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
+
 
 
 class SparkApp {
@@ -31,5 +33,24 @@ class SparkApp {
   //import spark.implicits._
   def sparkTest(): Unit ={
     println("Spark Test")
+/*  covid_19_data.csv"
+    time_series_covid_19_confirmed.csv"
+    projectdata/time_series_covid_19_confirmed_US.csv"
+    time_series_covid_19_deaths.csv"
+    time_series_covid_19_deaths_US.csv"
+    time_series_covid_19_recovered.csv"
+    */
+
+    //Test Dataframe to load HDFS
+    val userFile = sparkRun.read
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .option("sep", ",")
+      .option("nullValue", "")
+      .csv(hdfsLocation.hdfs_path + "covid_19_data.csv")
+      .toDF()
+
+    userFile.show()
   }
+
 }
